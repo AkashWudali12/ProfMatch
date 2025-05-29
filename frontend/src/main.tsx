29436a2f9e868534b87e331072d65e0ca5703466
AkashWudali12 <akash.wudali@gmail.com>
2025-05-29 01:21:59 -0400

    google analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { inject } from '@vercel/analytics'
 import './index.css'
 import Home from './pages/Home.tsx'
 
-inject()
+// TypeScript declaration for Google Analytics
+declare global {
+  interface Window {
+    dataLayer: unknown[];
+    gtag: (...args: unknown[]) => void;
+  }
+}
+
+const GA_MEASUREMENT_ID = "G-TKEPB5L1DE"
+
+const script = document.createElement('script')
+script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+script.async = true
+document.head.appendChild(script)
+
+window.dataLayer = window.dataLayer || []
+function gtag(...args: unknown[]) {
+  window.dataLayer.push(args)
+}
+gtag('js', new Date())
+gtag('config', GA_MEASUREMENT_ID)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
